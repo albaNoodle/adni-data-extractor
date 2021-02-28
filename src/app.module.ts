ec5a88b3a81fa6f6116127c4f43a263f2d52d6e8
alba <albiclevi@gmail.com>
2021-02-28 21:56:48 +0100

    Csv for tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdniReaderModule } from './adni-reader/adni-reader.module';
 
 @Module({
-  imports: [],
+  imports: [AdniReaderModule],
   controllers: [AppController],
   providers: [AppService],
 })
