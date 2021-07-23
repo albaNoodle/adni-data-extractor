915cd414af96c58f5b9675a6d2f6e843ed97ceb9
Alba <alba.clemente@microhealth.com>
2021-07-23 10:48:07 +0200

    [DB] Migration working

---
+++ migrations/1626915455223-initial.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class initial1626915455223 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // Adni Image
+        
+        await queryRunner.query(
+            'CREATE TABLE `adni_image` ( \
+              `id` int NOT NULL AUTO_INCREMENT, \
+              `imageUid` int NOT NULL, \
+              `patientId` int NOT NULL, \
+              `examDate` datetime NOT NULL, \
+              `visCode` varchar(16) NOT NULL, \
+              `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+              `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+              UNIQUE INDEX `IDX_78a916df40e02a9deb1c4b75ed` (`imageUid`), \
+              PRIMARY KEY (`id`) \
+              ) ENGINE=InnoDB',
+          );       
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TABLE `adni_image`');
+    }
+
+}
